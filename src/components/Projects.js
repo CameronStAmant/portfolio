54b2d5458ab305664ef2dfe8e83f6843f3ed70c9
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-09-14 14:48:38 -0600

    Add project listings

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,37 @@
 import './Projects.css';
+import blogScreenshot from '../assets/blogScreenshot.png';
+import battleshipScreenshot from '../assets/battleship-screenshot.png';
+import membersOnlyScreenshot from '../assets/members-only-screenshot.png';
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      Projects
+      <div>
+        <div>
+          <h2>Blog</h2>
+          <img src={blogScreenshot} alt="Screenshot of blog" />
+          <p>
+            Fully featured blog that allows posting, comments, as well as
+            moderation from admins.
+          </p>
+        </div>
+        <div>
+          <h2>Battleship</h2>
+          <img src={battleshipScreenshot} alt="Screenshot of battleship" />
+          <p>The game of battleship that you can take on the go!</p>
+        </div>
+        <div>
+          <h2>Members Only Message Board</h2>
+          <img
+            src={membersOnlyScreenshot}
+            alt="Screenshot of members only message board"
+          />
+          <p>A secret message board where only members know who wrote what.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
