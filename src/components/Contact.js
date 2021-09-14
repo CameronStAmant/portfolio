c9184aabe40b4d9538e2e960a976490091e52f31
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-09-14 10:17:52 -0600

    Add contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,26 @@
 import './Contact.css';
+import resume from '../assets/Cameron-St-Amant-Resume.pdf';
 
 function Contact() {
   return (
     <div className="Contact">
-      <div>Contact</div>
-      <div>cameronstamant@protonmail.com</div>
-      <div>Github</div>
-      <div>LinkedIn</div>
-      <div>Resume</div>
+      <div id="contact">Contact</div>
+      <div>
+        <a href="mailto:cameronstamant@protonmail.com?subject=I found your portfolio and I really like what I see">
+          cameronstamant@protonmail.com
+        </a>
+      </div>
+      <div>
+        <a href="https://github.com/CameronStAmant">Github</a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/cameron-stamant">LinkedIn</a>
+      </div>
+      <div>
+        <a href={resume} download>
+          Resume
+        </a>
+      </div>
     </div>
   );
 }
