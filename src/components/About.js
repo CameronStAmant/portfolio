ba6599c62ad5305c9a164d6a64c6e828370f077a
CameronStAmant <40364114+CameronStAmant@users.noreply.github.com>
2021-09-16 14:46:25 -0600

    Add more responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,11 @@ function About() {
     <div className="About">
       <div id="about">A little about me</div>
       <div id="aboutContent">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        I'm Cameron, a Software Engineer who, after over a year of study, is
+        ready to contribute to the world. My background in customer support is
+        where my interest in software development was born. Going from nothing
+        to full stack knowledge has been an exciting journey, one that is only
+        just beginning.
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,37 +3,19 @@ import blogScreenshot from '../assets/blogScreenshot.png';
 import battleshipScreenshot from '../assets/battleship-screenshot.png';
 import membersOnlyScreenshot from '../assets/members-only-screenshot.png';
 
-import React, { useState } from 'react';
-
 function Projects() {
-  const [gallery, setGallery] = useState(1);
-
-  const next = () => {
-    if (gallery === 3) {
-      setGallery(1);
-    } else {
-      setGallery(() => gallery + 1);
-    }
-  };
-
-  const previous = () => {
-    if (gallery === 1) {
-      setGallery(3);
-    } else {
-      setGallery(() => gallery - 1);
-    }
-  };
-
   return (
     <div className="projects">
-      <div id="projects">Projects</div>
-      <button onClick={previous}>Previous</button>
-      <button onClick={next}>Next</button>
+      <div id="projects">
+        <div>Some of my work</div>
+      </div>
       <div>
-        {gallery === 1 && (
+        <div className="project">
+          <h2 className="projectTitle">Blog</h2>
           <div>
-            <h2>Blog</h2>
             <img src={blogScreenshot} alt="Screenshot of blog" />
+          </div>
+          <div className="front">
             <p>
               Fully featured blog that allows posting, comments, as well as
               moderation from admins.
@@ -55,58 +37,62 @@ function Projects() {
               </a>
             </div>
           </div>
-        )}
-        {gallery === 2 && (
+        </div>
+        <div className="project">
           <div>
-            <h2>Battleship</h2>
+            <h2 className="projectTitle">Battleship</h2>
             <img src={battleshipScreenshot} alt="Screenshot of battleship" />
-            <p>The game of battleship that you can take on the go!</p>
-            <div className="projectButtons">
-              <a
-                href="https://github.com/CameronStAmant/battleship"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <button className="code">See GitHub</button>
-              </a>
-              <a
-                href="https://cameronstamant.github.io/battleship/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <button className="live">See Project</button>
-              </a>
+            <div className="front">
+              <p>The game of battleship that you can take on the go!</p>
+              <div className="projectButtons">
+                <a
+                  href="https://github.com/CameronStAmant/battleship"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="code">See GitHub</button>
+                </a>
+                <a
+                  href="https://cameronstamant.github.io/battleship/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="live">See Project</button>
+                </a>
+              </div>
             </div>
           </div>
-        )}
-        {gallery === 3 && (
+        </div>
+        <div className="project">
           <div>
-            <h2>Members Only Message Board</h2>
+            <h2 className="projectTitle">Members Only Message Board</h2>
             <img
               src={membersOnlyScreenshot}
               alt="Screenshot of members only message board"
             />
-            <p>
-              A secret message board where only members know who wrote what.
-            </p>
-            <div className="projectButtons">
-              <a
-                href="https://github.com/CameronStAmant/members-only-node"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <button className="code">See GitHub</button>
-              </a>
-              <a
-                href="https://secure-oasis-30404.herokuapp.com/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <button className="live">See Project</button>
-              </a>
+            <div className="front">
+              <p>
+                A secret message board where only members know who wrote what.
+              </p>
+              <div className="projectButtons">
+                <a
+                  href="https://github.com/CameronStAmant/members-only-node"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="code">See GitHub</button>
+                </a>
+                <a
+                  href="https://secure-oasis-30404.herokuapp.com/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  <button className="live">See Project</button>
+                </a>
+              </div>
             </div>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
